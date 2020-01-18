b1c542ba684b5e1a12a459df0e4a0e26db2d1a96
⭐ B3none <ablackham2000@gmail.com>
2020-01-18 02:58:59 +0000

    Remove redundant check because of redundant testing code.

--- src/bot.js
+++ src/bot.js
@@ -6,18 +6,11 @@ dotenv.config();
 
 const client = new Discord.Client();
 
-const updateStatus = () => {
-  query
-    .info(process.env.SERVER_IP, Number(process.env.SERVER_PORT), 2000)
-    .then(({playersnum, maxplayers}) => {
-      if (client.user) {
-        client.user.setActivity(`${playersnum} / ${maxplayers} Online`);
-      }
-    })
-    .catch(console.log);
-};
+const updateStatus = async () => {
+  const {playersnum, maxplayers} = await query.info(process.env.SERVER_IP, Number(process.env.SERVER_PORT), 2000);
 
-updateStatus();
+  client.user.setActivity(`${playersnum} / ${maxplayers} Online`);
+};
 
 client.on('ready', updateStatus);
 setInterval(updateStatus, process.env.UPDATE_STATUS_INTERVAL_MINUTES * 60 * 1000);
